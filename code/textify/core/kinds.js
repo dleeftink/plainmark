41690379d9ff1aaed8e4fe31be7cf34d42ec8d3c
dleeftink <dennisleeftink@gmail.com>
2024-10-25 12:56:33 +1000

    Sort tags by frequency

--- code/textify/core/kinds.js
+++ code/textify/core/kinds.js
@@ -2,17 +2,16 @@ export function kinds(tagName) {
 
   // how: to handle "*" exceptions? (spec)
   // how: to handle "!" exceptions? (mine)
-  // how: more optimal search 
+  // how: to more optimal search 
   
   let tags = {
-    phrasing: ["a*", "abbr", "area*", "audio", "b", "bdi", "bdo", "br", "button", "canvas", "cite", "code", "data", "date", "datalist", "del*", "dfn", "em", "embed", "i", "iframe", "img", "input", "ins*", "kbd", "keygen", "label", "link*", "map*", "mark", "math", "meta*", "meter", "noscript", "object", "output", "picture", "progress", "q", "ruby", "s", "samp", "script", "select", "slot", "small", "span", "strong", "sub", "sup", "svg", "template", "textarea", "time", "u", "var", "video", "wbr", "Text*"],
-    embedded: ["audio", "canvas", "embed", "iframe", "img", "math", "object", "picture", "svg", "video"],
+    phrasing: ["a*","span","img","br","script","meta*","link*","i","input","strong","b","label","button","svg","em","noscript","time","iframe","small","select","abbr","sup","ins*","u","picture","area*","code","textarea","video","cite","dfn","del*","date","keygen","Text*","s","wbr","sub","kbd","object","map*","var","embed","canvas","template","bdi","q","audio","mark","samp","ruby","bdo","data","meter","output","slot","progress","math","datalist"],    embedded: ["audio", "canvas", "embed", "iframe", "img", "math", "object", "picture", "svg", "video"],
     heading: ["h1", "h2", "h3", "h4", "h5", "h6", "hgroup"],
-    sectioning: ["article", "aside", "nav", "section"],
+    sectioning: ["section","article","nav","aside"],
     metadata: ["base", "link", "meta", "noscript", "script", "style", "template", "title"],
-    navigation: ["menu","nav","search"], // custom field;
-    interactive: ["a*", "audio*", "button", "details", "embed", "iframe", "img*", "input*", "keygen", "label", "object*", "select", "textarea", "video*"],
-    flow: ["a", "abbr", "address", "area*", "article", "aside", "audio", "b", "bdi", "bdo", "blockquote", "br", "button", "canvas", "cite", "code", "data", "date", "datalist", "del", "details", "dfn", "dialog", "div", "dl", "em", "embed", "fieldset", "figure", "footer", "form", "h1", "h2", "h3", "h4", "h5", "h6", "header", "hgroup", "hr", "i", "iframe", "img", "input", "ins", "kbd", "keygen", "label", "li*!", "link*", "main*", "map", "mark", "math", "menu", "meta*", "meter", "nav", "noscript", "object", "ol", "output", "p", "picture", "pre", "progress", "q", "ruby", "s", "samp", "script", "search", "section", "select", "slot", "small", "span", "strong", "sub", "sup", "svg", "table", "template", "textarea", "time", "u", "ul", "var", "video", "wbr", "Text*"],
+    navigation: ["nav,""menu","nav"], // custom field;
+    interactive: ["a*","img*","input*","label","button","iframe","select","textarea","video*","keygen","object*","embed","audio*","details"],
+    flow: ["div","a","span","li*!","img","br","p","script","ul","meta*","link*","i","input","strong","h2","h3","b","h4","label","table","button","svg","section","article","em","form","h1","noscript","header","time","figure","dl","h5","iframe","hr","footer","nav","small","aside","select","h6","abbr","sup","ins","u","ol","blockquote","picture","fieldset","area*","code","textarea","video","cite","dfn","main*","pre","del","address","date","keygen","search","Text*","s","wbr","sub","kbd","object","map","hgroup","var","embed","menu","canvas","template","bdi","q","audio","mark","details","samp","ruby","bdo","data","meter","output","slot","progress","dialog","math","datalist"],
   };
 
   let kind = Object.entries(tags)
